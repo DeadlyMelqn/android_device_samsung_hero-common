cd66596d98454049f265c93fe913a6149195917b
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-09-07 13:27:53 +0200

    hero: use Linaro 7.1.1 2017.08 as kernel toolchain
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,8 @@ TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 # Kernel
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_USES_UNCOMPRESSED_KERNEL := true
 
 # Partitions
