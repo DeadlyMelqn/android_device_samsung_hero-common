26cb8a45af4d6450dc10c6587d79a085f127e14a
Unknown <dailytroll92@gmail.com>
2017-09-23 16:58:09 -0600

    hero: media: add SOMX codecs
    Change-Id: Ia785d5e37cfdbb6d93c49b3bb315d15693fb7317

--- hero-common.mk
+++ hero-common.mk
@@ -144,6 +144,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
     $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media/somxreg.conf:system/etc/somxreg.conf
 
 # GPS
 PRODUCT_COPY_FILES += \
